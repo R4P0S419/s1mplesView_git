7b29d957b844a653675038fa2a23090726b15693
Raposa Foxyghostgm <r4p0s419@gmail.com>
2024-04-25 20:54:17 -0300

    primeiro commit do projeto

---
+++ main.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const nameElement = document.querySelector('#name');
+    const usernameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+    const linkElement = document.querySelector('#link');
+
+    const linkGit = prompt('Digite seu Username do git:')
+
+    const linkhit = `https://api.github.com/users/${linkGit}`
+
+    fetch(linkhit)
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(json) {
+        nameElement.innerText = json.name;
+        usernameElement.innerText = json.login;
+        avatarElement.src = json.avatar_url;
+        reposElement.innerText = json.public_repos;
+        followersElement.innerText = json.followers;
+        followingElement.innerText = json.following;
+        linkElement.href = json.html_url;
+    })
+})
